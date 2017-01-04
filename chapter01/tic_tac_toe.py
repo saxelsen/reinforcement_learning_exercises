40cb7ef7ab0869603d66b7bda766daa15e9c6e2f
Sebastian Axelsen <sa@palmtreestatistics.com>
2017-01-04 08:24:31 +0800

    Adds helper methods for converting boards in TicTacToe

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -20,30 +20,30 @@ def invert_state(state):
     return result
 
 
-def next_state(state):
-    result_state = list(state)
-    last_tile = EMPTY
-    index_of_next_empty = 0
-    for i, tile in enumerate(state):
-        if tile == EMPTY and last_tile != EMPTY:
-            index_of_next_empty = i
-            break
-        else:
-            last_tile = tile
-
-    result_state[index_of_next_empty] = X
-    return result_state
-
-
 def generate_state_space():
     state_space = dict()
-    initial_state = (EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY)
+    root_state = np.ones((ROWS, COLS)).astype(int) * EMPTY
+    root_state_key = state_to_string(root_state)
+    state_space[root_state_key] = 0.5
+
+    return state_space
+
+
+def state_to_string(state):
+    return ''.join(map(str, state.flatten().tolist()))
+
+
+def string_to_state(state):
+    if len(state) != 9:
+        raise ValueError('The input state must be 9 digits long.')
+    state_list = list(map(int, list(state)))
+    return np.array([state_list[:3], state_list[3:6], state_list[6:]])
 
 
 # Use numpy array to store the board state and then use this function to convert between a range index and coordinates.
 def index_to_board_coordinates(index):
     if index < 0 or index > 8:
-        raise IndexError("Index out of bounds. Index is bounded by 0 and 8")
+        raise IndexError('Index out of bounds. Index is bounded by 0 and 8')
     row = floor(index / COLS)
     col = index % 3
 
--- testing/chapter01/testing_tic_tac_toe.py
+++ testing/chapter01/testing_tic_tac_toe.py
@@ -1,4 +1,6 @@
 import unittest
+import numpy as np
+import numpy.testing as nptest
 from chapter01 import tic_tac_toe as ttt
 
 
@@ -20,6 +22,20 @@ class TestingTicTacToe(unittest.TestCase):
 
     def test_index_to_board_coordinates_oob_upper(self):
         self.assertRaises(IndexError, ttt.index_to_board_coordinates, 9)
+
+    def test_string_to_state(self):
+        string = '000000000'
+        expected = np.ones((3,3)).astype(int) * 0
+        state = ttt.string_to_state(string)
+        nptest.assert_array_almost_equal(expected, state)
+
+    def test_string_to_state_error(self):
+        string = '0'
+        self.assertRaises(ValueError, ttt.string_to_state, string)
+
+    def test_string_to_state_char(self):
+        string = 'WONT WORK'
+        self.assertRaises(ValueError, ttt.string_to_state, string)
 
 
 
