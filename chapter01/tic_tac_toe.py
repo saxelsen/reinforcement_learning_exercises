dcceb9c15ac30d3070f9851bbf7ce8780158d14c
Sebastian Axelsen <sa@palmtreestatistics.com>
2017-01-09 06:14:07 +0100

    Writes the first outline of a game playthrough. Not complete

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -4,12 +4,13 @@ from math import floor
 ROWS = 3
 COLS = 3
 
+#TODO: Wrap these in an Enum class
 EMPTY = 0
 X = 1
 O = 5
 
 START_PROB = 0.5  #The default probability estimate of winning is 50%
-
+EMPTY_BOARD = np.zeros((ROWS, COLS)).astype(int)
 
 def invert_state(state):
     result = list(map(int, list(state)))
@@ -25,7 +26,7 @@ def invert_state(state):
 
 def generate_state_space():
     state_space = dict()
-    root_state = [0] * (ROWS * COLS)
+    root_state = EMPTY_BOARD.flatten().tolist()
     root_state_key = list_to_string(root_state)
     state_space[root_state_key] = default_probability(root_state_key)
 
@@ -65,7 +66,7 @@ def add_states(root_state, player, state_space):
 
 
 def default_probability(state_string):
-    default_prob = 0.5
+    default_prob = START_PROB
     state = list(map(int, list(state_string)))
 
     x_wins = is_winning_state_for_player(state, X)
@@ -121,5 +122,76 @@ def index_to_board_coordinates(index):
     col = index % 3
 
     return row, col
+
+
+def print_board(board):
+    player_to_string = {EMPTY: ' ', X: 'X', O: 'O'}
+
+    for row in board:
+        row_string = '|'
+        for col in row:
+            row_string += player_to_string[col] + '|'
+        print(row_string)
+
+
+def play_game(ai_player, ai_model_one, ai_model_two=None):
+    """
+    Play through a game. X always starts the game.
+    :param ai_player: An int equivalent to X or O. If ai_player is O, the user plays X and vice-versa.
+    :param ai_model_one: The AI model to play against.
+    :param ai_model_two: A secondary AI model that can play the game. If this is not None, the two AIs will play each
+    other. Should be used for training models.
+    :return:
+    """
+
+    board = EMPTY_BOARD.copy()
+    is_game_won = is_game_finished = False
+    current_player = X
+    print_board(board)
+
+    while is_game_finished is not True:
+
+        if current_player == ai_player:
+            move = ai_model_one.move(board)
+        else:
+            if ai_model_two is None:
+                move = get_user_input(board)
+            else:
+                move = ai_model_two.move(board)
+
+        board[move] = current_player
+        print_board(board)
+
+        board_state = board.flatten().tolist()
+        is_game_won = is_winning_state_for_player(board_state, current_player)
+        is_game_finished = is_game_won or (EMPTY not in board_state)
+
+    if is_game_won:
+        player_string = 'X' if current_player == X else 'O'
+        print('Player {} won the game'.format(player_string))
+    else:
+        print('The game was a tie.')
+
+
+def input_to_move(user_input):
+    x, y = user_input.split(',')
+    return int(x), int(y)
+
+
+def get_user_input(board):
+    print('Please enter a move:')
+    user_in = input()
+    try:
+        x, y = input_to_move(user_in)
+    except Exception as e:
+        print('Move is invalid.')
+        x, y = get_user_input(board)
+
+    if not (0 <= x <= 2) or not (0 <= y <= 2) or (board[x, y] != EMPTY):
+        print('Move is invalid.')
+        x, y = get_user_input(board)
+
+    return x, y
+
 
 
