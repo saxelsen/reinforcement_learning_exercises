65e3fe69a1980580022aeb3c36324abd7c9af3df
Sebastian Axelsen <sa@palmtreestatistics.com>
2017-01-09 16:38:59 +0100

    Moves nearly all functions into classes for easier configuration

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -18,127 +18,131 @@ O = 5
 START_PROB = 0.5  #The default probability estimate of winning is 50%
 EMPTY_BOARD = np.zeros((ROWS, COLS)).astype(int)
 
-def invert_state(state):
-    result = list(map(int, list(state)))
+class TicTacToeUtils:
 
-    for tile, player in enumerate(result):
-        if player == EMPTY:
-            pass
+    @staticmethod
+    def is_winning_state_for_player(state, player):
+        winner = False
+        goal_sum = 3*player
+
+        if sum(state[:3]) == goal_sum or \
+           sum(state[3:6]) == goal_sum or \
+           sum(state[6:]) == goal_sum or \
+           sum([state[i] for i in [0, 3, 6]]) == goal_sum or \
+           sum([state[i] for i in [1, 4, 7]]) == goal_sum or \
+           sum([state[i] for i in [2, 5, 8]]) == goal_sum or \
+           sum([state[i] for i in [0, 4, 8]]) == goal_sum or \
+           sum([state[i] for i in [2, 4, 6]]) == goal_sum:
+            winner = True
+
+        return winner
+
+    @staticmethod
+    def list_to_string(a_list):
+        return ''.join(map(str, a_list))
+
+    @staticmethod
+    def state_to_string(state):
+        return ''.join(map(str, state.flatten().tolist()))
+
+    @staticmethod
+    def string_to_state(state):
+        if len(state) != 9:
+            raise ValueError('The input state must be 9 digits long.')
+        state_list = list(map(int, list(state)))
+        return np.array([state_list[:3], state_list[3:6], state_list[6:]])
+
+    @staticmethod
+    def index_to_board_coordinates(index):
+        if index < 0 or index > 8:
+            raise IndexError('Index out of bounds. Index is bounded by 0 and 8')
+        row = floor(index / COLS)
+        col = index % 3
+
+        return row, col
+
+    @staticmethod
+    def print_board(board):
+        player_to_string = {EMPTY: ' ', X: 'X', O: 'O'}
+
+        for row in board:
+            row_string = '|'
+            for col in row:
+                row_string += player_to_string[col] + '|'
+            print(row_string)
+        print('')
+
+
+class TicTacToeGenerator:
+
+    @staticmethod
+    def invert_state(state):
+        result = list(map(int, list(state)))
+
+        for tile, player in enumerate(result):
+            if player == EMPTY:
+                pass
+            else:
+                result[tile] = O if player == X else X
+
+        return TicTacToeUtils.list_to_string(result)
+
+    @staticmethod
+    def generate_state_space():
+        state_space = dict()
+        root_state = EMPTY_BOARD.flatten().tolist()
+        root_state_key = TicTacToeUtils.list_to_string(root_state)
+        state_space[root_state_key] = TicTacToeGenerator.default_probability(root_state_key)
+
+        TicTacToeGenerator.add_states(root_state, X, state_space)
+
+        for state_key in list(state_space.keys()):
+            inverted_state_key = TicTacToeGenerator.invert_state(state_key)
+            state_space[inverted_state_key] = TicTacToeGenerator.default_probability(inverted_state_key)
+
+        return state_space
+
+    @staticmethod
+    def add_states(root_state, player, state_space):
+        """
+        Go through all the tiles on the board. If there is an empty tile, fill it with the current player's symbol,
+        change the player and do repeat with the new board state until the entire board has been filled.
+        This will recursively fill the state_space from the given root_state.
+
+        :param root_state: a list of length 9 with each element representing a tile on the TicTacToe board.
+        :param player: an int: 1 or 2 for X or O
+        :param state_space: a dict containing the current game states as keys
+        :return: Nothing. Adds new elements to the state_space dict
+        """
+
+        for i, tile in enumerate(root_state):
+            if tile == EMPTY:
+                new_state = list(root_state)
+                new_state[i] = player
+                new_state_key = ''.join(map(str, new_state))
+                default_prob = TicTacToeGenerator.default_probability(new_state_key)
+                state_space[new_state_key] = default_prob
+
+                if 0 < default_prob < 1:
+                    next_player = O if player == X else X
+                    # Continue down the game tree if this game was not ended
+                    TicTacToeGenerator.add_states(new_state, next_player, state_space)
+
+    @staticmethod
+    def default_probability(state_string):
+        default_prob = START_PROB
+        state = list(map(int, list(state_string)))
+
+        x_wins = TicTacToeUtils.is_winning_state_for_player(state, X)
+
+        if x_wins:
+            default_prob = 1
         else:
-            result[tile] = O if player == X else X
+            o_wins = TicTacToeUtils.is_winning_state_for_player(state, O)
+            if o_wins:
+                default_prob = 0
 
-    return list_to_string(result)
-
-
-def generate_state_space():
-    state_space = dict()
-    root_state = EMPTY_BOARD.flatten().tolist()
-    root_state_key = list_to_string(root_state)
-    state_space[root_state_key] = default_probability(root_state_key)
-
-    add_states(root_state, X, state_space)
-
-    for state_key in list(state_space.keys()):
-        inverted_state_key = invert_state(state_key)
-        state_space[inverted_state_key] = default_probability(inverted_state_key)
-
-    return state_space
-
-
-def add_states(root_state, player, state_space):
-    """
-    Go through all the tiles on the board. If there is an empty tile, fill it with the current player's symbol,
-    change the player and do repeat with the new board state until the entire board has been filled.
-    This will recursively fill the state_space from the given root_state.
-
-    :param root_state: a list of length 9 with each element representing a tile on the TicTacToe board.
-    :param player: an int: 1 or 2 for X or O
-    :param state_space: a dict containing the current game states as keys
-    :return: Nothing. Adds new elements to the state_space dict
-    """
-
-    for i, tile in enumerate(root_state):
-        if tile == EMPTY:
-            new_state = list(root_state)
-            new_state[i] = player
-            new_state_key = ''.join(map(str, new_state))
-            default_prob = default_probability(new_state_key)
-            state_space[new_state_key] = default_prob
-
-            if 0 < default_prob < 1:
-                next_player = O if player == X else X
-                # Continue down the game tree if this game was not ended
-                add_states(new_state, next_player, state_space)
-
-
-def default_probability(state_string):
-    default_prob = START_PROB
-    state = list(map(int, list(state_string)))
-
-    x_wins = is_winning_state_for_player(state, X)
-
-    if x_wins:
-        default_prob = 1
-    else:
-        o_wins = is_winning_state_for_player(state, O)
-        if o_wins:
-            default_prob = 0
-
-    return default_prob
-
-
-def is_winning_state_for_player(state, player):
-    winner = False
-    goal_sum = 3*player
-
-    if sum(state[:3]) == goal_sum or \
-       sum(state[3:6]) == goal_sum or \
-       sum(state[6:]) == goal_sum or \
-       sum([state[i] for i in [0, 3, 6]]) == goal_sum or \
-       sum([state[i] for i in [1, 4, 7]]) == goal_sum or \
-       sum([state[i] for i in [2, 5, 8]]) == goal_sum or \
-       sum([state[i] for i in [0, 4, 8]]) == goal_sum or \
-       sum([state[i] for i in [2, 4, 6]]) == goal_sum:
-        winner = True
-
-    return winner
-
-
-def list_to_string(a_list):
-    return ''.join(map(str, a_list))
-
-
-def state_to_string(state):
-    return ''.join(map(str, state.flatten().tolist()))
-
-
-def string_to_state(state):
-    if len(state) != 9:
-        raise ValueError('The input state must be 9 digits long.')
-    state_list = list(map(int, list(state)))
-    return np.array([state_list[:3], state_list[3:6], state_list[6:]])
-
-
-# Use numpy array to store the board state and then use this function to convert between a range index and coordinates.
-# Only use it for active games.
-def index_to_board_coordinates(index):
-    if index < 0 or index > 8:
-        raise IndexError('Index out of bounds. Index is bounded by 0 and 8')
-    row = floor(index / COLS)
-    col = index % 3
-
-    return row, col
-
-
-def print_board(board):
-    player_to_string = {EMPTY: ' ', X: 'X', O: 'O'}
-
-    for row in board:
-        row_string = '|'
-        for col in row:
-            row_string += player_to_string[col] + '|'
-        print(row_string)
-    print('')
+        return default_prob
 
 
 def play_game(ai_player, ai_model_one, ai_model_two=None, training=False):
@@ -154,7 +158,7 @@ def play_game(ai_player, ai_model_one, ai_model_two=None, training=False):
     board = EMPTY_BOARD.copy()
     is_game_won = is_game_finished = False
     current_player = X
-    print_board(board)
+    TicTacToeUtils.print_board(board)
 
     while is_game_finished is not True:
 
@@ -177,9 +181,9 @@ def play_game(ai_player, ai_model_one, ai_model_two=None, training=False):
             if ai_model_two is not None:
                 ai_model_two.update(board_before, board)
 
-        print_board(board)
+        TicTacToeUtils.print_board(board)
         board_state = board.flatten().tolist()
-        is_game_won = is_winning_state_for_player(board_state, current_player)
+        is_game_won = TicTacToeUtils.is_winning_state_for_player(board_state, current_player)
         is_game_finished = is_game_won or (EMPTY not in board_state)
 
         if not is_game_finished:
@@ -217,7 +221,7 @@ class RLTicTacToe:
 
     def __init__(self, symbol, model=None, greedy_factor=0.9, learning_rate=0.5):
         if model is None:
-            self.model = generate_state_space()
+            self.model = TicTacToeGenerator.generate_state_space()
         else:
             self.model = model
 
@@ -237,7 +241,7 @@ class RLTicTacToe:
             if tile == EMPTY:
                 new_state = list(current_state)
                 new_state[i] = self.symbol
-                new_state_key = list_to_string(new_state)
+                new_state_key = TicTacToeUtils.list_to_string(new_state)
                 possible_plays[i] = self.model[new_state_key]
 
         sorted_possible_plays = sorted(possible_plays.items(), key=lambda x: x[1], reverse=True)
@@ -247,16 +251,16 @@ class RLTicTacToe:
         is_greedy = nprandom.rand() < self.greedy_factor
 
         if is_greedy:
-            result_move = index_to_board_coordinates(greedy_move[0])
+            result_move = TicTacToeUtils.index_to_board_coordinates(greedy_move[0])
         else:
             exploratory_move = random.choice(exploratory_moves)
-            result_move = index_to_board_coordinates(exploratory_move[0])
+            result_move = TicTacToeUtils.index_to_board_coordinates(exploratory_move[0])
 
         return result_move
 
     def update(self, old_board, new_board):
-        old_board_key = state_to_string(old_board)
-        new_board_key = state_to_string(new_board)
+        old_board_key = TicTacToeUtils.state_to_string(old_board)
+        new_board_key = TicTacToeUtils.state_to_string(new_board)
 
         new_board_value = self.model[new_board_key]
         old_board_value = self.model[old_board_key]
--- testing/chapter01/testing_tic_tac_toe.py
+++ testing/chapter01/testing_tic_tac_toe.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 import numpy.testing as nptest
 from chapter01 import tic_tac_toe as ttt
+from chapter01.tic_tac_toe import TicTacToeGenerator as tttgen, TicTacToeUtils as tttutils
 
 
 class TestTicTacToe(unittest.TestCase):
@@ -14,56 +15,56 @@ class TestTicTacToe(unittest.TestCase):
     def test_index_to_board_coordinates(self):
 
         for i in range(0,9):
-            coords = ttt.index_to_board_coordinates(i)
+            coords = tttutils.index_to_board_coordinates(i)
             self.assert_coordinate_bounds(coords)
 
     def test_index_to_board_coordinates_oob_lower(self):
-        self.assertRaises(IndexError, ttt.index_to_board_coordinates, -1)
+        self.assertRaises(IndexError, tttutils.index_to_board_coordinates, -1)
 
     def test_index_to_board_coordinates_oob_upper(self):
-        self.assertRaises(IndexError, ttt.index_to_board_coordinates, 9)
+        self.assertRaises(IndexError, tttutils.index_to_board_coordinates, 9)
 
     def test_string_to_state(self):
         string = '000000000'
         expected = np.ones((3,3)).astype(int) * 0
-        state = ttt.string_to_state(string)
+        state = tttutils.string_to_state(string)
         nptest.assert_array_almost_equal(expected, state)
 
     def test_string_to_state_error(self):
         string = '0'
-        self.assertRaises(ValueError, ttt.string_to_state, string)
+        self.assertRaises(ValueError, tttutils.string_to_state, string)
 
     def test_string_to_state_char(self):
         string = 'WONT WORK'
-        self.assertRaises(ValueError, ttt.string_to_state, string)
+        self.assertRaises(ValueError, tttutils.string_to_state, string)
 
     def test_is_winning_state_for_player(self):
         X = ttt.X
         EMPTY = ttt.EMPTY
 
         state = [X, X, X, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
         state = [EMPTY, EMPTY, EMPTY, X, X, X, EMPTY, EMPTY, EMPTY]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
         state = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, X, X, X]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
         state = [X, EMPTY, EMPTY, X, EMPTY, EMPTY, X, EMPTY, EMPTY]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
         state = [EMPTY, X, EMPTY, EMPTY, X, EMPTY, EMPTY, X, EMPTY]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
         state = [EMPTY, EMPTY, X, EMPTY, EMPTY, X, EMPTY, EMPTY, X]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
         state = [X, EMPTY, EMPTY, EMPTY, X, EMPTY, EMPTY, EMPTY, X]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
         state = [EMPTY, EMPTY, X, EMPTY, X, EMPTY, X, EMPTY, EMPTY]
-        self.assertTrue(ttt.is_winning_state_for_player(state, X))
+        self.assertTrue(tttutils.is_winning_state_for_player(state, X))
 
     def test_is_winning_state_for_player_false(self):
         X = ttt.X
@@ -71,13 +72,13 @@ class TestTicTacToe(unittest.TestCase):
         O = ttt.O
 
         state = [X, X, O, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
-        self.assertFalse(ttt.is_winning_state_for_player(state, X))
+        self.assertFalse(tttutils.is_winning_state_for_player(state, X))
 
         state = [EMPTY, EMPTY, EMPTY, EMPTY, X, X, EMPTY, EMPTY, EMPTY]
-        self.assertFalse(ttt.is_winning_state_for_player(state, X))
+        self.assertFalse(tttutils.is_winning_state_for_player(state, X))
 
         state = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
-        self.assertFalse(ttt.is_winning_state_for_player(state, X))
+        self.assertFalse(tttutils.is_winning_state_for_player(state, X))
 
 
 class TestRLTicTacToe(unittest.TestCase):
